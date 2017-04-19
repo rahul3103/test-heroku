df09f6dcbf5bdc3aa1204e51b3dc9e580a2f7372
Rahul Shrivastava <rahul3103@gmail.com>
2017-04-20 00:57:54 +0530

    test

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    a = 'Default'
+    b = 'Default'
+    if os.environ.get('DATABASE_URL'):
+        a = os.environ.get('DATABASE_URL')
+    if os.environ.get('HEROKU'):
+        b = os.environ.get('HEROKU')
+    return render_template('index.html', a=a, b=b)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
